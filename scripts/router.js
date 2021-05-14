c7b63dd1869574c6a483ec1cc0ceb7f3f76d430c
Ryan Shamoon <rshamoon@ucsd.edu>
2021-05-13 23:57:04 -0700

    first parts of part 1
    Co-authored-by: asya-balas <asya-balas@users.noreply.github.com>
    Co-authored-by: bgt002 <bgt002@users.noreply.github.com>

--- scripts/router.js
+++ scripts/router.js
@@ -1,11 +1,11 @@
 // router.js
 
-export const router = {};
-
+export const router = {
+};
 /**
  * Changes the "page" (state) that your SPA app is currently set to
  */
-router.setState = function() {
+router.setState = function(page, data) {
   /**
    * - There are three states that your SPA app will have
    *    1. The home page
@@ -35,4 +35,26 @@ router.setState = function() {
    *    1. You may add as many helper functions in this file as you like
    *    2. You may modify the parameters of setState() as much as you like
    */
+
+  //switch on page address
+  //set the proper document properties for each page type
+  //settings the class triggers the css
+
+  //settings
+  if(page == "/#settings"){
+    document.title = "settings";
+    document.body.className = "settings";
+    //sets h1
+    document.body.firstElementChild.firstElementChild.innerHTML = "Settings";
+  }
+  //individual entry
+  else if(page.search("entry") != -1){
+    document.body.className = "single-entry";
+  }
+  //homepage
+  else{
+    document.title = "Lab 6 - Web Components";
+    document.body.className = "";
+    //get and update h1 to say journal
+  }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -9,10 +9,37 @@ document.addEventListener('DOMContentLoaded', () => {
   fetch('https://cse110lab6.herokuapp.com/entries')
     .then(response => response.json())
     .then(entries => {
+      let counter = 0;
       entries.forEach(entry => {
         let newPost = document.createElement('journal-entry');
         newPost.entry = entry;
+       
+        //Set id for each entry
+        let id = counter;
+        counter++;
+
+        //add click functionality to each entry
+        newPost.addEventListener("click", () => {
+          //passes in entry and entry number to router
+          router.setState("/#entry" + id.toString(), entry);
+          history.pushState(entry, null, "/#entry" + id);
+        })
+
         document.querySelector('main').appendChild(newPost);
       });
     });
 });
+
+//pop the address of the previous location
+window.addEventListener("popstate", (e) => {
+  router.setState("/" + e.currentTarget.location.hash, null);
+});
+
+//adds click functionality to settings (triggers router)
+let body = document.querySelector("body");
+let settingsBtn = body.querySelector("body header img[alt='settings']");
+settingsBtn.addEventListener("click", () => {
+  //sends the page address to the router
+  router.setState("/#settings", null);
+  history.pushState(-1, null, "/#settings");
+})
